5015373932b86d580c76b5f65ba479b359efcc84
Harry Cutts <eternal.linux@gmail.com>
2014-01-02 15:26:17 +0000

    Fix normal shaders to use normals from meshes
    (Instead of assuming a sphere, as before.)

--- shaders/normals/geometry.glsl
+++ shaders/normals/geometry.glsl
@@ -2,16 +2,17 @@
 layout (points) in;
 layout (line_strip, max_vertices = 2) out;
 
-in vec3 msPoint[];
+in vec3 msPosition2[];
+in vec3 msNormal2[];
 uniform mat4 MVP;
 
 void main() {
 	// Emit the vertex itself as the start of the line
-	gl_Position = MVP * vec4(msPoint[0], 1.0);
+	gl_Position = MVP * vec4(msPosition2[0], 1.0);
 	EmitVertex();
 
 	// Emit the end point of the line
-	gl_Position = MVP * (vec4(msPoint[0] * 1.2, 1.0));
+	gl_Position = MVP * (vec4(msPosition2[0] + msNormal2[0], 1.0));
 	EmitVertex();
 
 	EndPrimitive();
--- shaders/normals/vertex.glsl
+++ shaders/normals/vertex.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 in vec3 msPosition;
-out vec3 msPoint;    // The point which goes into the geometry shader
+in vec3 msNormal;
+out vec3 msPosition2;    // The point which goes into the geometry shader
+out vec3 msNormal2;
 
 void main() {
-	msPoint = msPosition;
+	msPosition2 = msPosition;
+	msNormal2   = msNormal;
 }
